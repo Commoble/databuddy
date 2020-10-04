c9c6e0bf765a11b435a3a1a31611f0e87ecbddb1
Commoble <josephbettendorff@yahoo.com>
2020-10-04 13:49:10 -0500

    Fix javadocs

--- src/main/java/commoble/databuddy/data/CodecJsonDataManager.java
+++ src/main/java/commoble/databuddy/data/CodecJsonDataManager.java
@@ -48,9 +48,9 @@ import net.minecraft.util.ResourceLocation;
  * <p>See drullkus's primer on what codecs are and how to assemble them:<br>
  * https://gist.github.com/Drullkus/1bca3f2d7f048b1fe03be97c28f87910</p>
  * 
- * Usage for this is as follows:</p>
+ * <p>Usage for this is as follows:</p>
  * 
- * A) Make a codec for your json<br>
+ * <p>A) Make a codec for your json<br>
  * e.g. if you have a json like</p>
  * 
  * <pre>
@@ -72,9 +72,9 @@ import net.minecraft.util.ResourceLocation;
  * // use the ResourceLocation codec to create a Block codec
  * Codec{@literal <Block>} BLOCK_CODEC = ResourceLocation.CODEC.xmap(ForgeRegistries.BLOCKS::getValue, Block::getRegistryName);
  * // use that to build a codec for BlockHolder. RecordCodecBuilder builds codecs that deserialize {}objects or CompoundNBTs.
- * Codec{@literal <BlockHolder>} BLOCK_HOLDER_CODEC = RecordCodecBuilder.create(instance -> instance.group(
+ * Codec{@literal <BlockHolder>} BLOCK_HOLDER_CODEC = RecordCodecBuilder.create(instance {@literal ->} instance.group(
  * 		// we can stuff up to 16 fields into the group method, using a codec for each field...
- * 		BLOCK_CODEC.fieldOf("block").forGetter(blockHolder -> blockHolder.block)
+ * 		BLOCK_CODEC.fieldOf("block").forGetter(blockHolder {@literal ->} blockHolder.block)
  * 	).apply(instance, BlockHolder::new); // use the constructor to finish assembling the codec
  * </pre>
  * 
@@ -86,14 +86,14 @@ import net.minecraft.util.ResourceLocation;
  * 	event.addListener(BLOCK_HOLDERS);
  * }
  * </pre>
- * The above event runs during server init. The data manager's map will be recreated during server init and when
+ * <p>The above event runs during server init. The data manager's map will be recreated during server init and when
  * somebody uses the /reload command. Storing this in a static field should be safe in most cases, but if somebody
  * were to somehow be running multiple servers at once then problems may occur.</p>
  * 
- * Finally, we can use BLOCK_HOLDERS.getData("some_modid:some_json") to get the data
+ * <p>Finally, we can use BLOCK_HOLDERS.getData("some_modid:some_json") to get the data
  * represented by the json at data/modid/block_holders/some_json.json</p>
  * 
- * The folder name that we specified as "block_holders" can potentially include subfolders in the name as well,
+ * <p>The folder name that we specified as "block_holders" can potentially include subfolders in the name as well,
  * so we (the people who made the data loader) could use our own modid as part of the folder name to avoid
  * name collisions, e.g. using "blockholdermod/block_holders" as the folder name would result in a json with the
  * id "bananas:banana_block" to be located at data/bananas/blockholdermod/block_holders/some_json.json</p>
@@ -116,8 +116,8 @@ public class CodecJsonDataManager<T> extends JsonReloadListener
 	
 	/**
 	 * Creates a data manager with a standard gson parser
-	 * @param folderName The name of the data folder that we will load from, vanilla folderNames are "recipes", "loot_tables", etc</br>
-	 * Jsons will be read from data/all_modids/folderName/all_jsons</br>
+	 * @param folderName The name of the data folder that we will load from, vanilla folderNames are "recipes", "loot_tables", etc<br>
+	 * Jsons will be read from data/all_modids/folderName/all_jsons<br>
 	 * folderName can include subfolders, e.g. "some_mod_that_adds_lots_of_data_loaders/cheeses"
 	 * @param codec A codec to deserialize the json into your T, see javadocs above class
 	 * @param logger A logger that will log json parsing problems when they are caught.
@@ -129,8 +129,8 @@ public class CodecJsonDataManager<T> extends JsonReloadListener
 	
 	/**
 	 * As above but with a custom GSON
-	 * @param folderName The name of the data folder that we will load from, vanilla folderNames are "recipes", "loot_tables", etc</br>
-	 * Jsons will be read from data/all_modids/folderName/all_jsons</br>
+	 * @param folderName The name of the data folder that we will load from, vanilla folderNames are "recipes", "loot_tables", etc<br>
+	 * Jsons will be read from data/all_modids/folderName/all_jsons<br>
 	 * folderName can include subfolders, e.g. "some_mod_that_adds_lots_of_data_loaders/cheeses"
 	 * @param codec A codec to deserialize the json into your T, see javadocs above class
 	 * @param logger A logger that will log json parsing problems when they are caught.
