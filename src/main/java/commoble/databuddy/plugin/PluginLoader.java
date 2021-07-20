1bdf4b46f28097e5e64e2910673a7c07726b99e8
Commoble <josephbettendorff@yahoo.com>
2021-07-20 11:16:38 -0500

    Add a new PluginLoader#loadPlugins overload that returns failure results instead of accepting a logger, deprecated old method
    Better for API users to log faults themselves

--- src/main/java/commoble/databuddy/plugin/PluginLoader.java
+++ src/main/java/commoble/databuddy/plugin/PluginLoader.java
@@ -31,12 +31,16 @@ THE SOFTWARE.
 
 package commoble.databuddy.plugin;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
-import java.util.stream.Stream;
 
 import org.apache.logging.log4j.Logger;
 import org.objectweb.asm.Type;
+
+import com.mojang.datafixers.util.Either;
+import com.mojang.datafixers.util.Pair;
 
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.forgespi.language.ModFileScanData.AnnotationData;
@@ -61,7 +65,9 @@ public class PluginLoader
 	 * @param logger A logger to log plugin-loading information with
 	 * @param loggerName A name used by the logger, can be your modid
 	 * @return The registrator instance that was given, after 
+	 * @deprecated TODO will be removed in 1.18.x; use {@link PluginLoader#loadPlugins(Class, Class, Object)} instead
 	 */
+	@Deprecated
 	public static <REGISTRATOR, PLUGIN extends Consumer<REGISTRATOR>, REGISTRY extends REGISTRATOR> REGISTRY loadPlugins(
 		final Class<?> annotationClass,
 		final Class<PLUGIN> pluginInterface,
@@ -70,8 +76,45 @@ public class PluginLoader
 		final String loggerName)
 	{
 		logger.info("Loading {} plugins", loggerName);
+		
+		List<Pair<String,Exception>> failures = loadPlugins(annotationClass, pluginInterface, registrator);
+		
+		for (Pair<String,Exception> errorEntry : failures)
+		{
+			logger.error("Plugin loader for {} failed to load a plugin {} due to exception:", loggerName, errorEntry.getFirst());
+			errorEntry.getSecond().printStackTrace();
+		}
+		
+		logger.info("Completed loading of {} plugins", loggerName);
 
+		return registrator;
+	}
+
+	/**
+	 * <p>Gathers all classes that implement the given interface and are annotated with the given annotation,
+	 * attempts to make one instance of each of these classes, and registers them to some plugin-registering object.</p>
+	 * 
+	 * <p>In this manner, a mod that uses the plugin loader can grant API access to other mods if the API mod is present.</p>
+	 * 
+	 * <p>Classes that implement the given PLUGIN interface and are annotated with the given annotation must have an argless
+	 * constructor, or that plugin will fail to load. Plugins that fail to load will be caught and logged.</p>
+	 * 
+	 * @param <REGISTRATOR> Interface that plugins can consume
+	 * @param <PLUGIN> Plugin interface type. Must be able to consume REGISTRATORS.
+	 * @param <REGISTRY> Implementation type of REGISTRATOR to register plugins to
+	 * @param annotationClass Annotation class that the class scanner will look for
+	 * @param pluginInterface Registrator-consuming interface class
+	 * @param registrator The object to give to plugins so they can register things to it
+	 * @return A list of failure results; each entry contains the class name of a plugin class that could not be instantiated,
+	 * and an exception that occurred when trying to instantiate it
+	 */
+	public static <REGISTRATOR, PLUGIN extends Consumer<REGISTRATOR>, REGISTRY extends REGISTRATOR> List<Pair<String,Exception>> loadPlugins(
+		final Class<?> annotationClass,
+		final Class<PLUGIN> pluginInterface,
+		final REGISTRY registrator)
+	{
 		final Type annotationType = Type.getType(annotationClass);
+		final List<Pair<String,Exception>> failures = new ArrayList<>();
 
 		// get the names of all classes annotated with the plugin annotation
 		ModList.get().getAllScanData().stream().flatMap(modData -> modData.getAnnotations().stream())
@@ -79,38 +122,35 @@ public class PluginLoader
 			.map(AnnotationData::getMemberName)
 			
 			// try to create instances of these classes
-			.flatMap(className -> PluginLoader.createPluginInstance(pluginInterface, className, logger, loggerName))
-			// and allow them to register circuit behaviors if they were instantiated
-			// successfully
-			.forEach(plugin -> plugin.accept(registrator));
-
-		logger.info("Completed loading of {} plugins", loggerName);
-
-		return registrator;
+			.map(className -> PluginLoader.createPluginInstance(pluginInterface, className))
+			// and allow them to register behaviors if they were instantiated successfully
+			.forEach(either ->
+			{
+				either.left().ifPresent(plugin -> plugin.accept(registrator));
+				either.right().ifPresent(failures::add);
+			});
+		
+		return failures;
 	}
 	
 	/**
 	 * Attempts to create a plugin instance, given the name of the class to instantiate.
-	 * We use a Stream instead of Optional so the mod scan stream can flatmap it.
 	 * @param pluginInterface The interface that plugin classes have implemented
 	 * @param className The fully-qualified class name of the plugin implementation class
-	 * @param logger the logger
-	 * @param loggerName the name of the logger
-	 * @return A Stream containing the instance if successful, or an empty stream otherwise.
+	 * @return An Either containing either the newly created plugin instance if successful, or a pair of the className and a thrown exception if unsuccessful 
 	 */
-	private static <PLUGIN> Stream<PLUGIN> createPluginInstance(Class<PLUGIN> pluginInterface, String className, Logger logger, String loggerName)
+	private static <PLUGIN> Either<PLUGIN, Pair<String,Exception>> createPluginInstance(Class<PLUGIN> pluginInterface, String className)
 	{
 		try
 		{
-			return Stream.of(
+			return Either.left(
 				Class.forName(className) // get the exact class by name
 				.asSubclass(pluginInterface) // as a subclass of Plugin
 				.newInstance()); // and try to instantiate it via its argless constructor
 		}
 		catch (Exception e)
 		{
-			logger.error("Failed to load {} Plugin: {}", loggerName, className, e);
-			return Stream.empty();
+			return Either.right(Pair.of(className,e));
 		}
 	}
 }
