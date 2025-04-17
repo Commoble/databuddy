be9e67490a59d74f61b89e1681388ba818b811cf
Commoble <josephbettendorff@yahoo.com>
2025-04-17 18:23:07 -0400

    Fix blockstate datagen

--- src/examplemod/java/net/commoble/databuddy/examplecontent/BlockStateDataGenExampleMod.java
+++ src/examplemod/java/net/commoble/databuddy/examplecontent/BlockStateDataGenExampleMod.java
@@ -2,23 +2,16 @@ package net.commoble.databuddy.examplecontent;
 
 import java.util.HashMap;
 
+import com.mojang.math.Quadrant;
 import com.mojang.serialization.JsonOps;
 
-import net.commoble.databuddy.datagen.BlockStateFile;
-import net.commoble.databuddy.datagen.BlockStateFile.Case;
-import net.commoble.databuddy.datagen.BlockStateFile.Model;
-import net.commoble.databuddy.datagen.BlockStateFile.Multipart;
-import net.commoble.databuddy.datagen.BlockStateFile.OrCase;
-import net.commoble.databuddy.datagen.BlockStateFile.PropertyValue;
-import net.commoble.databuddy.datagen.BlockStateFile.Variants;
-import net.commoble.databuddy.datagen.BlockStateFile.WhenApply;
+import net.commoble.databuddy.datagen.BlockStateBuilder;
 import net.commoble.databuddy.datagen.SimpleModel;
 import net.minecraft.Util;
-import net.minecraft.client.resources.model.BlockModelRotation;
-import net.minecraft.core.registries.Registries;
+import net.minecraft.client.renderer.block.model.BlockModelDefinition;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.random.Weighted;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RedStoneWireBlock;
@@ -43,23 +36,20 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 @EventBusSubscriber(modid=DataBuddyExampleMod.MODID, bus=Bus.MOD)
 public class BlockStateDataGenExampleMod
 {
-	private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Registries.BLOCK, DataBuddyExampleMod.MODID);
-	private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, DataBuddyExampleMod.MODID);
-	private static final DeferredHolder<Block,Block> RANDOM_SPONGE = BLOCKS.register("random_sponge", () ->
-		new Block(BlockBehaviour.Properties.of()));
-	private static final DeferredHolder<Block,SlabBlock> SPONGE_SLAB = BLOCKS.register("sponge_slab", () ->
-		new SlabBlock(BlockBehaviour.Properties.of()));
-	private static final DeferredHolder<Block,RedStoneWireBlock> WHITESTONE_DUST = BLOCKS.register("whitestone_wire", () ->
-		new RedStoneWireBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.REDSTONE_WIRE)));
+	private static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(DataBuddyExampleMod.MODID);
+	private static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(DataBuddyExampleMod.MODID);
+	private static final DeferredHolder<Block,Block> RANDOM_SPONGE = BLOCKS.registerBlock("random_sponge", Block::new);
+	private static final DeferredHolder<Block,SlabBlock> SPONGE_SLAB = BLOCKS.registerBlock("sponge_slab", SlabBlock::new);
+	private static final DeferredHolder<Block,RedStoneWireBlock> WHITESTONE_DUST = BLOCKS.registerBlock("whitestone_wire", RedStoneWireBlock::new, BlockBehaviour.Properties.ofFullCopy(Blocks.REDSTONE_WIRE));
 	
 	static
 	{
-		ITEMS.register("random_sponge", () ->
-			new BlockItem(RANDOM_SPONGE.get(), new Item.Properties()));
-		ITEMS.register("sponge_slab", () ->
-			new BlockItem(SPONGE_SLAB.get(), new Item.Properties()));
-		ITEMS.register("whitestone", () ->
-			new BlockItem(WHITESTONE_DUST.get(), new Item.Properties()));
+		ITEMS.registerItem("random_sponge", props ->
+			new BlockItem(RANDOM_SPONGE.get(), props));
+		ITEMS.registerItem("sponge_slab", props ->
+			new BlockItem(SPONGE_SLAB.get(), props));
+		ITEMS.registerItem("whitestone", props ->
+			new BlockItem(WHITESTONE_DUST.get(), props));
 	}
 	
 	@SubscribeEvent
@@ -70,7 +60,7 @@ public class BlockStateDataGenExampleMod
 		ITEMS.register(modBus);
 	}
 	@SubscribeEvent
-	public static void onGatherData(GatherDataEvent event)
+	public static void onGatherData(GatherDataEvent.Client event)
 	{
 		ResourceLocation blockSpongeSlab = ResourceLocation.fromNamespaceAndPath(DataBuddyExampleMod.MODID, "block/sponge_slab");
 		ResourceLocation blockSpongeSlabTop = ResourceLocation.fromNamespaceAndPath(DataBuddyExampleMod.MODID, "block/sponge_slab_top");
@@ -106,78 +96,49 @@ public class BlockStateDataGenExampleMod
 		}));
 
 		// blockstate generation examples
-		BlockStateFile.addDataProvider(event, DataBuddyExampleMod.MODID, JsonOps.INSTANCE, Util.make(new HashMap<ResourceLocation, BlockStateFile>(), map ->
+		BlockStateBuilder.addDataProvider(event, Util.make(new HashMap<ResourceLocation, BlockModelDefinition>(), map ->
 		{
 			// random models example
-			map.put(RANDOM_SPONGE.getId(),
-				BlockStateFile.variants(
-					Variants.always(
-						Model.create(blockSponge),
-						Model.create(blockSponge, BlockModelRotation.X0_Y90),
-						Model.create(blockSponge, BlockModelRotation.X0_Y180),
-						Model.create(blockSponge, BlockModelRotation.X0_Y270))));
+			map.put(RANDOM_SPONGE.getId(), BlockStateBuilder.singleVariant(BlockStateBuilder.randomModels(
+				new Weighted<>(BlockStateBuilder.model(blockSponge), 1),
+				new Weighted<>(BlockStateBuilder.model(blockSponge, Quadrant.R0, Quadrant.R90), 1),
+				new Weighted<>(BlockStateBuilder.model(blockSponge, Quadrant.R0, Quadrant.R180), 1),
+				new Weighted<>(BlockStateBuilder.model(blockSponge, Quadrant.R0, Quadrant.R270), 1))));
 			
 			// variant example
-			map.put(SPONGE_SLAB.getId(),
-				BlockStateFile.variants(Variants.builder()
-					.addVariant(
-						PropertyValue.create(SlabBlock.TYPE, SlabType.BOTTOM),
-						Model.create(blockSpongeSlab))
-					.addVariant(
-						PropertyValue.create(SlabBlock.TYPE, SlabType.DOUBLE),
-						Model.create(blockSponge))
-					.addVariant(
-						PropertyValue.create(SlabBlock.TYPE, SlabType.TOP),
-						Model.create(blockSpongeSlabTop))));
+			map.put(SPONGE_SLAB.getId(), BlockStateBuilder.variants(variants -> variants
+				.addVariant(SlabBlock.TYPE, SlabType.BOTTOM, BlockStateBuilder.model(blockSpongeSlab))
+				.addVariant(SlabBlock.TYPE, SlabType.DOUBLE, BlockStateBuilder.model(blockSponge))
+				.addVariant(SlabBlock.TYPE, SlabType.TOP, BlockStateBuilder.model(blockSpongeSlabTop))));
 			
 			// multipart example
-			map.put(WHITESTONE_DUST.getId(),
-				BlockStateFile.multipart(Multipart.builder()
-					.addWhenApply(WhenApply.or(
-						OrCase.builder()
-							.addCase(Case.builder()
-								.addCondition(RedStoneWireBlock.EAST, RedstoneSide.NONE)
-								.addCondition(RedStoneWireBlock.NORTH, RedstoneSide.NONE)
-								.addCondition(RedStoneWireBlock.SOUTH, RedstoneSide.NONE)
-								.addCondition(RedStoneWireBlock.WEST, RedstoneSide.NONE))
-							.addCase(Case.builder()
-								.addCondition(RedStoneWireBlock.EAST, RedstoneSide.SIDE, RedstoneSide.UP)
-								.addCondition(RedStoneWireBlock.NORTH, RedstoneSide.SIDE, RedstoneSide.UP))
-							.addCase(Case.builder()
-								.addCondition(RedStoneWireBlock.EAST, RedstoneSide.SIDE, RedstoneSide.UP)
-								.addCondition(RedStoneWireBlock.SOUTH, RedstoneSide.SIDE, RedstoneSide.UP))
-							.addCase(Case.builder()
-								.addCondition(RedStoneWireBlock.SOUTH, RedstoneSide.SIDE, RedstoneSide.UP)
-								.addCondition(RedStoneWireBlock.WEST, RedstoneSide.SIDE, RedstoneSide.UP))
-							.addCase(Case.builder()
-								.addCondition(RedStoneWireBlock.NORTH, RedstoneSide.SIDE, RedstoneSide.UP)
-								.addCondition(RedStoneWireBlock.WEST, RedstoneSide.SIDE, RedstoneSide.UP)),
-						Model.create(ResourceLocation.withDefaultNamespace("block/redstone_dust_dot"))))
-					.addWhenApply(WhenApply.when(
-						Case.create(RedStoneWireBlock.NORTH, RedstoneSide.SIDE, RedstoneSide.UP),
-						Model.create(ResourceLocation.withDefaultNamespace("block/redstone_dust_side0"))))
-					.addWhenApply(WhenApply.when(
-						Case.create(RedStoneWireBlock.SOUTH, RedstoneSide.SIDE, RedstoneSide.UP),
-						Model.create(ResourceLocation.withDefaultNamespace("block/redstone_dust_side_alt0"))))
-					.addWhenApply(WhenApply.when(
-						Case.create(RedStoneWireBlock.EAST, RedstoneSide.SIDE, RedstoneSide.UP),
-						Model.create(ResourceLocation.withDefaultNamespace("block/redstone_dust_side_alt1"), BlockModelRotation.X0_Y270)))
-					.addWhenApply(WhenApply.when(
-						Case.create(RedStoneWireBlock.WEST, RedstoneSide.SIDE, RedstoneSide.UP),
-						Model.create(ResourceLocation.withDefaultNamespace("block/redstone_dust_side1"), BlockModelRotation.X0_Y270)))
-					.addWhenApply(WhenApply.when(
-						Case.create(RedStoneWireBlock.NORTH, RedstoneSide.UP),
-						Model.create(ResourceLocation.withDefaultNamespace("block/redstone_dust_up"))))
-					.addWhenApply(WhenApply.when(
-						Case.create(RedStoneWireBlock.EAST, RedstoneSide.UP),
-						Model.create(ResourceLocation.withDefaultNamespace("block/redstone_dust_up"), BlockModelRotation.X0_Y90)))
-					.addWhenApply(WhenApply.when(
-						Case.create(RedStoneWireBlock.SOUTH, RedstoneSide.UP),
-						Model.create(ResourceLocation.withDefaultNamespace("block/redstone_dust_up"), BlockModelRotation.X0_Y180)))
-					.addWhenApply(WhenApply.when(
-						Case.create(RedStoneWireBlock.WEST, RedstoneSide.UP),
-						Model.create(ResourceLocation.withDefaultNamespace("block/redstone_dust_up"), BlockModelRotation.X0_Y270)))));
-			
+			map.put(WHITESTONE_DUST.getId(), BlockStateBuilder.multipart(multipart -> multipart
+				.applyWhenOr(BlockStateBuilder.model(ResourceLocation.withDefaultNamespace("block/redstone_dust_dot")), or -> or
+					.addAllPropertiesSubWhen(when -> when
+						.addCondition(RedStoneWireBlock.EAST, RedstoneSide.NONE)
+						.addCondition(RedStoneWireBlock.NORTH, RedstoneSide.NONE)
+						.addCondition(RedStoneWireBlock.SOUTH, RedstoneSide.NONE)
+						.addCondition(RedStoneWireBlock.WEST, RedstoneSide.NONE))
+					.addAllPropertiesSubWhen(when -> when
+						.addCondition(RedStoneWireBlock.EAST, RedstoneSide.SIDE, RedstoneSide.UP)
+						.addCondition(RedStoneWireBlock.NORTH, RedstoneSide.SIDE, RedstoneSide.UP))
+					.addAllPropertiesSubWhen(when -> when
+						.addCondition(RedStoneWireBlock.EAST, RedstoneSide.SIDE, RedstoneSide.UP)
+						.addCondition(RedStoneWireBlock.SOUTH, RedstoneSide.SIDE, RedstoneSide.UP))
+					.addAllPropertiesSubWhen(when -> when
+						.addCondition(RedStoneWireBlock.SOUTH, RedstoneSide.SIDE, RedstoneSide.UP)
+						.addCondition(RedStoneWireBlock.WEST, RedstoneSide.SIDE, RedstoneSide.UP))
+					.addAllPropertiesSubWhen(when -> when
+						.addCondition(RedStoneWireBlock.NORTH, RedstoneSide.SIDE, RedstoneSide.UP)
+						.addCondition(RedStoneWireBlock.WEST, RedstoneSide.SIDE, RedstoneSide.UP)))
+				.applyWhen(BlockStateBuilder.model(ResourceLocation.withDefaultNamespace("block/redstone_dust_side0")), RedStoneWireBlock.NORTH, RedstoneSide.SIDE, RedstoneSide.UP)
+				.applyWhen(BlockStateBuilder.model(ResourceLocation.withDefaultNamespace("block/redstone_dust_side_alt0")), RedStoneWireBlock.SOUTH, RedstoneSide.SIDE, RedstoneSide.UP)
+				.applyWhen(BlockStateBuilder.model(ResourceLocation.withDefaultNamespace("block/redstone_dust_side_alt1"), Quadrant.R0, Quadrant.R270), RedStoneWireBlock.EAST, RedstoneSide.SIDE, RedstoneSide.UP)
+				.applyWhen(BlockStateBuilder.model(ResourceLocation.withDefaultNamespace("block/redstone_dust_side1"), Quadrant.R0, Quadrant.R270), RedStoneWireBlock.WEST, RedstoneSide.SIDE, RedstoneSide.UP)
+				.applyWhen(BlockStateBuilder.model(ResourceLocation.withDefaultNamespace("block/redstone_dust_up")), RedStoneWireBlock.NORTH, RedstoneSide.UP)
+				.applyWhen(BlockStateBuilder.model(ResourceLocation.withDefaultNamespace("block/redstone_dust_up"), Quadrant.R0, Quadrant.R90), RedStoneWireBlock.EAST, RedstoneSide.UP)
+				.applyWhen(BlockStateBuilder.model(ResourceLocation.withDefaultNamespace("block/redstone_dust_up"), Quadrant.R0, Quadrant.R180), RedStoneWireBlock.SOUTH, RedstoneSide.UP)
+				.applyWhen(BlockStateBuilder.model(ResourceLocation.withDefaultNamespace("block/redstone_dust_up"), Quadrant.R0, Quadrant.R270), RedStoneWireBlock.WEST, RedstoneSide.UP)));			
 		}));
 	}
 }
